ae0ed8ae80c45ab25b50701a854489d858840f26
exnwt <zeus02loz@gmail.com>
2025-03-05 15:51:38 +0700

    24

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,24 +11,6 @@
 
 <head>
     <!-- Basic -->
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
-    <!-- Site -->
-    <link rel="icon" href="{{ asset('frontend/images/fevicon.png')}}" type="image/gif" />
-    <meta name="keywords" content="" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>@yield('title', 'Osmaro')</title>
-
-    <!-- Stylesheets -->
-    <link href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap" rel="stylesheet" />
-    <link href="{{ asset('frontend/css/style.css')}}" rel="stylesheet" />
-    <link href="{{ asset('frontend/css/responsive.css')}}" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
 </head>
 
 <body>
--- routes/web.php
+++ routes/web.php
@@ -16,19 +16,21 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-
-
 Route::get('/', function () {
-    $site_name = "puki";
-    $lokasi = "meki"
-    $instagram = "aaa"
-    $facebook = "sss"
-    $linkedin = "dddd"
-    $post = "qwwww";
-
-    return view('home', compact('site_name', 'lokasi', 'instagram', 'facebook', 'linkedin', 'post'));
+    return view('home');
 });
+
+
+// Route::get('/', function () {
+//     $site_name = get_pengaturan_value('_site_name');
+//     $lokasi = get_pengaturan_value('_location');
+//     $instagram = get_pengaturan_value('_instagram');
+//     $facebook = get_pengaturan_value('_facebook');
+//     $linkedin = get_pengaturan_value('_linkedin');
+//     $post = get_post();
+
+//     return view('home', compact('site_name', 'lokasi', 'instagram', 'facebook', 'linkedin', 'post'));
+// });
 
 Route::get('/posts/{slug}', [TestController::class, 'show'])->name('posts.show');
 
