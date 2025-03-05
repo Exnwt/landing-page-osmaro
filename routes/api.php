504ca55512d9158885cc9277a4f63179e6771bfd
exnwt <zeus02loz@gmail.com>
2025-03-05 15:20:44 +0700

    17

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
-
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
 // Route::get('/', function () {
 //     $site_name = get_pengaturan_value('_site_name');
