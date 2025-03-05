5b6b65b0e7bac024284f072ea64430ebede94f8c
exnwt <zeus02loz@gmail.com>
2025-03-05 16:03:00 +0700

    28

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,10 +33,10 @@
             <div class="row">
                 <div class="col-lg-6">
                     <div class="image-grid">
-                        <div class="image-box"><img src="{{ asset('frontend/images/layanan1.png') }}" alt="Medical Check Up"></div>
-                        <div class="image-box"><img src="{{ asset('frontend/images/layanan2.jpg') }}" alt="Drug Test"></div>
-                        <div class="image-box"><img src="{{ asset('frontend/images/layanan3.jpg') }}" alt="Elderly Check Up"></div>
-                        <div class="image-box"><img src="{{ asset('frontend/images/layanan4.png') }}" alt="Lab Testing"></div>
+                        <div class="image-box"><img alt="Medical Check Up"></div>
+                        <div class="image-box"><img alt="Drug Test"></div>
+                        <div class="image-box"><img alt="Elderly Check Up"></div>
+                        <div class="image-box"><img alt="Lab Testing"></div>
                     </div>
                 </div>
                 <div class="col-lg-6">
