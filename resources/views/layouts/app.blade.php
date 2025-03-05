4fb80cdf9029ae6993c246e0b798f0819cb8da01
exnwt <zeus02loz@gmail.com>
2025-03-05 16:00:00 +0700

    26

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,6 +32,22 @@
 </head>
 
 <body>
-    <h1>Welcome to Home Page</h1>
+    <div class="hero_area">
+        @include('partials.header', ['site_name' => $site_name, 'instagram' => $instagram, 'facebook' => $facebook])
+
+        <main>
+            @yield('content') <!-- Konten dari halaman lain akan ditampilkan di sini -->
+        </main>
+    </div>
+
+    @include('partials.footer', ['site_name' => $site_name, 'instagram' => $instagram, 'facebook' => $facebook, 'linkedin' => $linkedin, 'lokasi' => $lokasi])
+
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="{{ asset('frontend/js/jquery-3.4.1.min.js')}}"></script>
+    <script src="{{ asset('frontend/js/bootstrap.js')}}"></script>
+
+    @yield('scripts')
 </body>
 </html>
